b2ad2eeef59e00f60c4f62ef0ed81eb300734aa6
enmccloud <66036830+enmccloud@users.noreply.github.com>
2020-07-26 12:52:53 -0500

    TagHelper

---
+++ AgileTicketSystem/AgileTicketSystem/TagHelper/RequiredTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgileTicketSystem.TagHelper;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AgileTicketSystem.TagHelper
+{
+    [HtmlTargetElement ("input", Attributes = "my-required")]
+    public class RequiredTagHelper : TagHelper
+    {
+
+        public override void Process (TagHelperContext context, TagHelperOutput output)
+        {
+            //add CSS Class to input
+            output.Attributes.AppendCssClass("form-control");
+
+            // Create <span> element
+            TagBuilder span = new TagBuilder("span");
+            span.Attributes.Add("class", "text-danger mr-2");
+            span.InnerHtml.Append("*");
+
+            //Add span after input
+            output.PostElement.AppendHtml(span);
+        }
+    }
+}
