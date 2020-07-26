41c67f3e823873c4d1d4c08018bae6c3fa2b6a42
enmccloud <66036830+enmccloud@users.noreply.github.com>
2020-07-26 13:05:21 -0500

    TagHelpers

---
+++ AgileTicketSystem/AgileTicketSystem/TagHelper/SubmitButtonTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AgileTicketSystem.TagHelper
+{
+    [HtmlTargetElement("button", Attributes = "submit")]
+    public class SubmitButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            object style = output.Attributes.ContainsName("style") ? output.Attributes["style"].Value : null;
+            output.Attributes.SetAttribute("style", style?.ToString());
+        }
+    }
+}
---
+++ AgileTicketSystem/AgileTicketSystem/ViewComponent/StatusViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AgileTicketSystem.Models;
+
+namespace AgileTicketSystem.ViewComponent
+{
+    public class StatusViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(Ticket ticket)
+        {
+            return View(ticket);
+        }
+    }
+}
